0def2909bed61f984fd43fb8d375de77ca8cc58a
Shayant Sital <shayant@gmail.com>
2021-02-08 03:12:21 -0300

    added margin in contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
       <h3 className="text-5xl lg:text-7xl text-blue-200 font-bold">
         Get in Touch
       </h3>
-      <p className="text-blue-200 text-center text-lg w-80">
+      <p className="mt-3 text-blue-200 text-center text-lg w-80">
         Although I'm not currently looking for any new opportunities, my inbox
         is always open. Whether you have a question or just want to say hi, I'll
         try my best to get back to you!
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="py-6 px-3 mt-3 bg-navy-light flex justify-center">
-      <p className="text-blue-200 text-md">Designed & Built by Shayant Sital</p>
+    <footer className="py-6 px-3 mt-3 bg-navy-light flex justify-center font-mono">
+      <p className="text-blue-200 text-">
+        Designed & Built by <strong>Shayant Sital</strong>
+      </p>
     </footer>
   );
 };
