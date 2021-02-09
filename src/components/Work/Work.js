6e160c50a6b777eff6faccb5f8502f12e0dc3d19
Shayant Sital <shayant@gmail.com>
2021-02-08 23:15:00 -0300

    added wokr section animation

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,13 +1,43 @@
-import React, { useState } from "react";
-import { motion, useCycle } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Work = () => {
   const [selectedJob, setselectedJob] = useState(0);
-  const [animate, cycle] = useCycle("{ opacity: [0, 1] }");
 
-  const workDetailVariant = {
+  const controls = useAnimation();
+  const { ref, inView } = useInView();
+
+  const varients = {
+    hidden: {
+      opacity: 0,
+    },
     visible: {
-      opacity: [0, 1],
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5,
+      },
+    },
+  };
+
+  const listVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+    },
+  };
+
+  const detailsVariants = {
+    hidden: {
+      x: "100vw",
+    },
+    visible: {
+      x: 0,
+      transition: {
+        ease: "easeInOut",
+      },
     },
   };
 
@@ -46,50 +76,58 @@ const Work = () => {
       ],
     },
   ];
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+    if (!inView) {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
   return (
-    <div className=" lg:flex flex-col lg:flex-row gap-8">
-      <div className="">
-        <ul className="inline-flex lg:block">
+    <motion.div
+      ref={ref}
+      className=" lg:flex flex-col lg:flex-row gap-8"
+      initial="hidden"
+      animate={controls}
+      variants={varients}
+    >
+      <div>
+        <motion.ul className="inline-flex lg:block">
           {work.map((work, index) => (
-            <li
+            <motion.li
               onClick={() => {
                 setselectedJob(index);
-                cycle();
               }}
               key={index}
+              variants={listVariants}
               className={`py-2 px-3 text-blue-200 m-0 p-0  rounded-sm border-t-2 lg:border-l-2 lg:border-t-0 cursor-pointer ${
                 index === selectedJob ? "border-red-400" : "border-blue-200"
               } hover:bg-navy-light  hover:text-white transition delay-200 ease-in`}
             >
               {work.name}
-            </li>
+            </motion.li>
           ))}
-        </ul>
+        </motion.ul>
       </div>
-      <motion.div
-        className="flex-auto mt-5 lg:mt-0"
-        animate={animate}
-        transition={{ duration: 2 }}
-      >
-        <motion.h3 className="text-blue-100 text-xl" initial={{ opacity: 0 }}>
+      <motion.div variants={detailsVariants} className="flex-auto mt-5 lg:mt-0">
+        <motion.h3 className="text-blue-100 text-xl">
           {work[selectedJob].position}{" "}
           <span className="text-red-400">@ {work[selectedJob].name}</span>
         </motion.h3>
-        <motion.p
-          className="text-blue-200 text-sm opacity-25"
-          animate={animate}
-        >
+        <motion.p className="text-blue-200 text-sm opacity-25">
           {work[selectedJob].start} - {work[selectedJob].end ?? "present"}
         </motion.p>
         <ul className=" py-3 text-blue-200">
           {work[selectedJob].keyPoints.map((point) => (
-            <motion.li className="py-3 max-w-lg" animate={animate}>
+            <motion.li className="py-3 max-w-lg">
               <span className="text-red-400 mr-2">▹</span> {point}
             </motion.li>
           ))}
         </ul>
       </motion.div>
-    </div>
+    </motion.div>
   );
 };
 
