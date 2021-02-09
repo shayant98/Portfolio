8ae36a93e4892a53d77842894618d3b50f7bf859
Shayant Sital <shayant@gmail.com>
2021-02-09 11:07:44 -0300

    added smoothscroll to top on logo click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = ({ toggleMenu, isOpen }) => {
     <>
       <nav className="bg-navy-normal fixed h-16 lg:h-24 w-screen justify-between flex items-center font-mono text-white text-md px-3 lg:px-12 z-50">
         <motion.div animate={{ opacity: 2 }}>
-          <Link to="/" className="pl-8">
+          <Link to="home" smooth={true} className="pl-8 cursor-pointer">
             LOGO
           </Link>
         </motion.div>
