bcacf5d854566b4f86d522e2b8536beb3829eb4c
Shayant Sital <shayant@gmail.com>
2021-02-08 23:04:11 -0300

    added about screen animation with inview hook

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,10 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 const About = () => {
+  const controls = useAnimation();
+  const { ref, inView } = useInView();
+
   const technologies = [
     "Javascript (ES6+)",
     "HTML & (S)CSS",
@@ -9,34 +13,87 @@ const About = () => {
     "Node.Js",
   ];
 
+  const varients = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const textVarients = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+    },
+  };
+
+  const imageVariants = {
+    hidden: {
+      x: "100vw",
+    },
+    visible: {
+      x: "0vw",
+    },
+  };
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+    if (!inView) {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
   return (
-    <div className="grid  lg:grid-cols-2 gap-4">
-      <div className="leading-6 text-blue-100  mt-7 font-sans">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-        <p className="pt-5">
+    <motion.div
+      ref={ref}
+      className="grid  lg:grid-cols-2 gap-4"
+      initial="hidden"
+      animate={controls}
+      variants={varients}
+    >
+      <motion.div className="leading-6 text-blue-100  mt-7 font-sans">
+        <motion.p variants={textVarients}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+        </motion.p>
+        <motion.p variants={textVarients} className="pt-5">
           Suspendisse faucibus ac metus viverra pharetra. Quisque non magna
           iaculis, cursus mauris ac, fermentum sapien. Praesent euismod sapien
           ac purus fringilla iaculis. Etiam a tempor mauris, in vehicula libero.
           Sed tempus elit nec libero vulputate, sit amet viverra augue
           tincidunt. Phasellus at ipsum nibh. Cras
-        </p>
-        <p className="pt-5">
+        </motion.p>
+        <motion.p variants={textVarients} className="pt-5">
           Cras mi est, pretium vel risus at, venenatis tincidunt orci. Nullam
           eget nulla vel mauris egestas convallis vitae quis nunc. Curabitur vel
           tristique lorem
-        </p>
-        <p className="pt-5">
+        </motion.p>
+        <motion.p variants={textVarients} className="pt-5">
           Here are a few technologies I've been working with recently:
-        </p>
-        <ul className="pt-8 grid grid-cols-2 text-sm">
+        </motion.p>
+        <motion.ul
+          variants={textVarients}
+          className="pt-8 grid grid-cols-2 text-sm"
+        >
           {technologies.map((technology, index) => (
-            <li key={index}>
+            <motion.li key={index} variants={textVarients}>
               <span className="text-red-400 mr-2">▹</span> {technology}
-            </li>
+            </motion.li>
           ))}
-        </ul>
-      </div>
-      <div className="group relative h-40 md:h-64 lg:h-80 w-max place-self-center cursor-pointer transition ease-in duration-100">
+        </motion.ul>
+      </motion.div>
+      <motion.div
+        variants={imageVariants}
+        className="group relative h-40 md:h-64 lg:h-80 w-max place-self-center cursor-pointer transition ease-in duration-100"
+      >
         <img
           src="https://loremflickr.com/640/360"
           alt="headhsot"
@@ -44,8 +101,8 @@ const About = () => {
         />
         <div className="h-full w-full z-0 absolute top-0 z-20 bg-opacity-75 bg-navy-light rounded-sm group-hover:bg-opacity-0 transition ease-in duration-300"></div>
         <div className="h-full w-full z-0 absolute top-4 left-4 lg:top-10 lg:left-10 border-4 border-navy-light rounded-sm group-hover:border-red-400  transition ease-in duration-400"></div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
+import { motion, useCycle } from "framer-motion";
 
 const Work = () => {
   const [selectedJob, setselectedJob] = useState(0);
+  const [animate, cycle] = useCycle("{ opacity: [0, 1] }");
+
+  const workDetailVariant = {
+    visible: {
+      opacity: [0, 1],
+    },
+  };
 
   const work = [
     {
@@ -21,9 +29,9 @@ const Work = () => {
       start: "Nov 2018",
       end: null,
       keyPoints: [
-        "My goal is to always build products that provide pixel-perfect, performant experiences.",
-        "My goal is to always build products that provide pixel-perfect, performant experiences.",
-        "My goal is to always build products that provide pixel-perfect, performant experiences.",
+        "My goal is to always build products that provide pixel-perfect, performant experiences. 2",
+        "My goal is to always build products that provide pixel-perfect, performant experiences. 2",
+        "My goal is to always build products that provide pixel-perfect, performant experiences. 2",
       ],
     },
     {
@@ -32,9 +40,9 @@ const Work = () => {
       start: "Nov 2018",
       end: null,
       keyPoints: [
-        "My goal is to always build products that provide pixel-perfect, performant experiences.",
-        "My goal is to always build products that provide pixel-perfect, performant experiences.",
-        "My goal is to always build products that provide pixel-perfect, performant experiences.",
+        "My goal is to always build products that provide pixel-perfect, performant experiences. 3",
+        "My goal is to always build products that provide pixel-perfect, performant experiences. 3",
+        "My goal is to always build products that provide pixel-perfect, performant experiences. 3",
       ],
     },
   ];
@@ -44,7 +52,10 @@ const Work = () => {
         <ul className="inline-flex lg:block">
           {work.map((work, index) => (
             <li
-              onClick={() => setselectedJob(index)}
+              onClick={() => {
+                setselectedJob(index);
+                cycle();
+              }}
               key={index}
               className={`py-2 px-3 text-blue-200 m-0 p-0  rounded-sm border-t-2 lg:border-l-2 lg:border-t-0 cursor-pointer ${
                 index === selectedJob ? "border-red-400" : "border-blue-200"
@@ -55,22 +66,29 @@ const Work = () => {
           ))}
         </ul>
       </div>
-      <div className="flex-auto mt-5 lg:mt-0">
-        <h3 className="text-blue-100 text-xl">
+      <motion.div
+        className="flex-auto mt-5 lg:mt-0"
+        animate={animate}
+        transition={{ duration: 2 }}
+      >
+        <motion.h3 className="text-blue-100 text-xl" initial={{ opacity: 0 }}>
           {work[selectedJob].position}{" "}
           <span className="text-red-400">@ {work[selectedJob].name}</span>
-        </h3>
-        <p className="text-blue-200 text-sm opacity-25">
+        </motion.h3>
+        <motion.p
+          className="text-blue-200 text-sm opacity-25"
+          animate={animate}
+        >
           {work[selectedJob].start} - {work[selectedJob].end ?? "present"}
-        </p>
+        </motion.p>
         <ul className=" py-3 text-blue-200">
           {work[selectedJob].keyPoints.map((point) => (
-            <li className="py-3 max-w-lg">
+            <motion.li className="py-3 max-w-lg" animate={animate}>
               <span className="text-red-400 mr-2">▹</span> {point}
-            </li>
+            </motion.li>
           ))}
         </ul>
-      </div>
+      </motion.div>
     </div>
   );
 };
