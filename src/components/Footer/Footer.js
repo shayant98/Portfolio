49b75186d932378f36b0e70d4891a829f35618d9
Shayant Sital <shayant@gmail.com>
2021-02-10 03:49:51 -0300

    only show social icons in footer on mobile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { useMedia } from "react-media";
 import SocialList from "./SocialList";
 
 const Footer = () => {
+  const isMobileScreen = useMedia({ query: "(max-width: 640px)" });
   return (
     <footer className="py-6 px-3 mt-3 bg-navy-normal flex flex-col items-center justify-center font-mono">
       <p className="text-blue-200 text-">
         Designed & Built by <strong>Shayant Sital</strong>
       </p>
-      <SocialList />
+      {isMobileScreen && <SocialList />}
     </footer>
   );
 };
