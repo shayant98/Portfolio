cca605669b1c83d0de2b3e2afcbf2febee786ddd
Shayant Sital <shayant@gmail.com>
2021-02-10 03:32:29 -0300

    switched hero hand svg with icon

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Button from "../Button";
+import { HiOutlineHand } from "react-icons/hi";
 const Hero = () => {
   return (
     <>
@@ -45,25 +46,7 @@ const Hero = () => {
           transition={{ delay: 1.3, duration: 0.5 }}
           href="mailto:shayant98@gmail.com"
         >
-          <Button
-            title="Get in touch"
-            icon={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                width="20px"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11"
-                />
-              </svg>
-            }
-          />
+          <Button title="Get in touch" icon={<HiOutlineHand />} />
         </motion.a>
       </div>
     </>
