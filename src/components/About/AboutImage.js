3ac42ab755b7b0c249819e2af93899d8436e7894
Shayant Sital <shayant@gmail.com>
2021-02-12 04:45:36 -0300

    added portrait image

--- src/components/About/AboutImage.js
+++ src/components/About/AboutImage.js
@@ -9,19 +9,23 @@ const AboutImage = () => {
     },
     visible: {
       x: "0vw",
+      transition: {
+        duration: 0.1,
+      },
     },
   };
   return (
     <motion.div
+      whileHover={{ scale: 1.05 }}
       variants={imageVariants}
-      className="group relative h-36 md:h-64 lg:h-80 w-max lg:ml-5 transition ease-in duration-100"
+      className="group relative h-80 mx-auto w-max lg:ml-5 transition ease-in duration-100"
     >
       <LazyLoadImage
-        src="./images/covid.png"
-        alt="headhsot"
-        className="z-10 relative h-full object-cover rounded-sm"
+        src="./images/portrait.jpg"
+        alt="headshot wearing yellow shirt"
+        className="z-10 relative  w-64 md:w-96 lg:w-96 h-full  object-cover object-top rounded-sm"
       />
-      <div className="h-full w-full z-0 absolute top-0 z-20 bg-opacity-75 bg-navy-light rounded-sm group-hover:bg-opacity-0 transition ease-in duration-300"></div>
+      <div className="h-full w-full z-0 absolute top-0 z-20 bg-opacity-75 bg-navy-normal rounded-sm group-hover:bg-opacity-0 transition ease-in duration-300"></div>
       <motion.div className="h-full w-full z-0 absolute top-4 left-4 border-4  border-red-400  rounded-sm transition ease-in duration-400"></motion.div>
     </motion.div>
   );
