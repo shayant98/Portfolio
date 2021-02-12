29ae1eaeb92155428fffd149d5bc9a62b9de1b88
Shayant Sital <shayant@gmail.com>
2021-02-12 04:33:46 -0300

    reduced animtion time on work list item

--- src/components/Work/WorkList/WorkListItem.js
+++ src/components/Work/WorkList/WorkListItem.js
@@ -21,7 +21,7 @@ const WorkListItem = ({ selectedJob, setselectedJob, index, job }) => {
         variants={listVariants}
         className={`py-2 px-3 text-blue-200 m-0 p-0  rounded-sm border-t-2 lg:border-l-2 lg:border-t-0 cursor-pointer ${
           index === selectedJob ? "border-red-400" : "border-blue-200"
-        } hover:bg-navy-light  hover:text-white transition delay-200 ease-in`}
+        } hover:bg-navy-light  hover:text-white transition delay-100 ease-in`}
       >
         {job.name}
       </motion.li>
