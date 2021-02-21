f9a0e08844fb54b9e87aabe427e63d2ec15d99f5
Shayant Sital <shayant@gmail.com>
2021-02-20 22:30:19 -0300

    changed mobiel menu width from full to 75% chanegd size of side menu items

--- src/components/Sidemenu/SideMenuListItem.js
+++ src/components/Sidemenu/SideMenuListItem.js
@@ -5,17 +5,19 @@ import { Link } from "react-scroll";
 const SideMenuListItem = ({ variants, index, option, toggleMenu }) => {
   return (
     <motion.div
+      className="flex flex-col items-center text-base"
       key={index}
       variants={variants}
       transition={{ delay: index * 0.2, type: "tween" }}
     >
+      <span className="font-mono text-sm text-red-400">0{index + 1}.</span>
       <Link
         to={option.to}
         smooth={true}
         className="cursor-pointer"
         onClick={() => toggleMenu(false)}
       >
-        <span className="text-red-400">0{index + 1}.</span> {option.title}
+        {option.title}
       </Link>
     </motion.div>
   );
--- src/components/Sidemenu/Sidemenu.js
+++ src/components/Sidemenu/Sidemenu.js
@@ -15,16 +15,25 @@ const Sidemenu = ({ isOpen, toggleMenu }) => {
   return (
     <AnimatePresence exitBeforeEnter>
       {isOpen ? (
-        <motion.div
-          variants={backdropVariants}
-          initial="hidden"
-          animate="visible"
-          className="bg-navy-normal  min-h-screen h-full w-full absolute  right-0  z-40 shadow-lg"
-        >
-          <motion.div className="bg-navy-normal  min-h-screen h-full w-full absolute  right-0  z-40 shadow-lg ">
-            <SideMenuList toggleMenu={toggleMenu} />
+        <div>
+          <motion.div
+            onClick={() => toggleMenu(false)}
+            variants={backdropVariants}
+            initial="hidden"
+            animate="visible"
+            className="bg-navy-normal  min-h-screen h-full w-full bg-opacity-75 absolute  right-0  z-40 shadow-lg"
+          ></motion.div>
+          <motion.div
+            variants={backdropVariants}
+            initial="hidden"
+            animate="visible"
+            className="bg-navy-normal  min-h-screen h-full w-3/4 absolute  right-0  z-40 shadow-lg"
+          >
+            <motion.div className="bg-navy-normal  min-h-screen h-full w-full absolute  right-0  z-40 shadow-lg ">
+              <SideMenuList toggleMenu={toggleMenu} />
+            </motion.div>
           </motion.div>
-        </motion.div>
+        </div>
       ) : (
         <div></div>
       )}
