61e5f0913c390211bcefdea9d0ae1dee8617ae0a
Shayant Sital <shayant@gmail.com>
2021-10-23 02:28:28 -0300

    fixed email

--- components/Email/Email.tsx
+++ components/Email/Email.tsx
@@ -5,10 +5,10 @@ const Email = () => {
     <div className="fixed left text-white bottom-0 right-5 w-12  hidden md:block" aria-orientation="vertical">
       <div className="flex flex-col items-center relative">
         <a
-          href="mailto:me@shayantsital.io"
+          href="mailto:me@shayantsital.com"
           className="writing-mode-vertical text-navy-light font-mono text-md leading-5 tracking-widest hover:text-red-400 transition delay-300 ease-in"
         >
-          me@shayantsital.io
+          me@shayantsital.com
         </a>
         <div className="h-32 border-l-2 border-white text-center mt-4 "></div>
       </div>
